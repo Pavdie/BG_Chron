5c91448a5243a664036f0d1e7a0c00c9f1d37521
Pavdie <pavdykova@gmail.com>
2024-08-17 04:31:28 +0300

    Quartz sync: Aug 17, 2024, 4:31 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -54,7 +54,7 @@ const config: QuartzConfig = {
   },
   plugins: {
     transformers: [
-      Plugin.FrontMatter({ language: toml} ),
+      Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "filesystem"],
       }),
